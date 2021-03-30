bb92f87660b3470af9a8e4893ca585a87d2a8544
Mario Rubiales <mariorubiales@gmail.com>
2021-03-30 20:48:07 +0200

    Modificación de las rutas
    Se ha modificado el fichero app-routing para incluir las rutas de los nuevos módulos e incluir la guarda de autenticación. A partir de ahora sólo se podrá acceder a la ruta /principal si el usuario ha iniciado sesión, en caso contrario se hará una redirección a /login.
    Del mismo modo, anteriormente si se accedía a "/" nos mandaba al login y ahora nos mandará /principal en caso de que el usuario ya haya iniciado sesión.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthenticatedGuard } from "./core/guards/authenticated.guard";
 // Components
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'principal', loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule) }
-  // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
+  {
+    path: "",
+    loadChildren: () =>
+      import(`./pages/principal/principal.module`).then(
+        (m) => m.PrincipalModule
+      ),
+    canActivate: [AuthenticatedGuard],
+    runGuardsAndResolvers: "always",
+  },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./pages/login/login.module").then((m) => m.LoginModule),
+  },
+  {
+    path: "register",
+    loadChildren: () =>
+      import("./pages/register/register.module").then((m) => m.RegisterModule),
+  },
+  {
+    path: "principal",
+    loadChildren: () =>
+      import(`./pages/principal/principal.module`).then(
+        (m) => m.PrincipalModule
+      ),
+    canActivate: [AuthenticatedGuard],
+    runGuardsAndResolvers: "always",
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
