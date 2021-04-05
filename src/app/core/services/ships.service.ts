3ea3dfa01b958d260f3b60bad837b98b5e2803ad
Mario Rubiales <mariorubiales@gmail.com>
2021-04-05 02:36:23 +0200

    Imágenes en las cards de la naves
    Se implementan dos nuevos métodos en ships service: 
    - getStarShipId: Obtiene el id de la nave de la URL obtenida por la API swapi
    - fillStarShipsPicURL: Rellena el valor del campo picURL de la lista de naves obtenida

--- src/app/core/services/ships.service.ts
+++ src/app/core/services/ships.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { from, Observable } from "rxjs";
 import { ApiHttpClientService } from "../services/api-http-client.service";
 import { environment } from "../../../environments/environment";
+import { Starship } from "../models/starship";
 
 @Injectable({
   providedIn: "root",
@@ -14,5 +15,25 @@ export class ShipsService {
   public getShips(pageNumber?: number): Observable<any> {
     const params: any = pageNumber ? { page: pageNumber } : {};
     return this.apiHttpClientService.get(this.starShipsURL, params);
+  }
+
+  public fillStarShipsPicURL(starShipList: Starship[]): Starship[] {
+    starShipList
+      .filter((starShip: Starship) => !starShip.picURL)
+      .map((starShip: Starship) => {
+        const starShipId: number = this.getStarShipId(starShip);
+        starShip.picURL = `${environment.starWarsVisualGuideURL}/${starShipId}.jpg`;
+
+        return starShip;
+      });
+
+    return starShipList;
+  }
+
+  public getStarShipId(starShip: Starship): number {
+    const arrangedURL: string = starShip.url.replace(/\/+$/, "");
+    const slicesURL = arrangedURL.split("/");
+
+    return parseInt(slicesURL.pop());
   }
 }
