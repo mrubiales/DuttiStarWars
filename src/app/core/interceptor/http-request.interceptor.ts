6df87e6a53c8c9ae81ac8c14c80efa84d5fea344
Mario Rubiales <mariorubiales@gmail.com>
2021-04-05 02:43:26 +0200

    Implementación de cache cliente en el interceptor
    Modificado el http-interceptor para añadir la gestión de una chache basada en los valores enviados por cabecera.

--- src/app/core/interceptor/http-request.interceptor.ts
+++ src/app/core/interceptor/http-request.interceptor.ts
@@ -4,15 +4,19 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpResponse,
 } from "@angular/common/http";
 
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { ApiHttpClientService } from "../services/api-http-client.service";
 import { environment } from "../../../environments/environment";
 import { AuthService } from "../services/auth.service";
+import { share, tap } from "rxjs/operators";
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  private cachedData = new Map<string, HttpResponse<any>>();
+
   constructor(
     private authService: AuthService,
     private apiHttpClientService: ApiHttpClientService
@@ -22,14 +26,46 @@ export class HttpRequestInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    console.log(request);
     const apiCredentials: string = this.apiHttpClientService.apiCredentials;
     if (request.headers.has(apiCredentials)) {
-      const headers = request.headers.delete(apiCredentials);
-      request = request.clone({ headers });
+      request = this.deleteHeader(request, apiCredentials);
       request = this.addAuthenticationToken(request);
     }
 
-    return next.handle(request);
+    if (request.method !== "GET" || !request.headers.has("x-cache")) {
+      return next.handle(request);
+    }
+
+    request = this.deleteHeader(request, "x-cache");
+    const cachedResponse: HttpResponse<any> = this.cachedData.get(
+      request.urlWithParams
+    );
+    if (cachedResponse) {
+      console.log("RESPONSE CACHED", this.cachedData);
+      return of(cachedResponse.clone());
+    }
+
+    return next.handle(request).pipe(
+      tap((httpEvent: any) => {
+        if (httpEvent instanceof HttpResponse) {
+          this.cachedData.set(request.urlWithParams, httpEvent.clone());
+        }
+        console.log("TAP CACHE", this.cachedData);
+      }),
+      share()
+    );
+  }
+
+  private deleteHeader(
+    request: HttpRequest<any>,
+    headerName: string
+  ): HttpRequest<any> {
+    if (request.headers.has(headerName)) {
+      const headers = request.headers.delete(headerName);
+      request = request.clone({ headers });
+    }
+    return request;
   }
 
   private addAuthenticationToken(request: HttpRequest<any>): HttpRequest<any> {
